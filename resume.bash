4cdf9f56d101b771120252ea2d654f40b0c69216
Simon Niklaus <simon.niklaus@outlook.com>
2025-07-04 15:55:48 -0700

    no message

--- resume.bash
+++ resume.bash
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-# https://registry.hub.docker.com/r/bwits/pdf2htmlex
-
-docker run -ti --rm -v $(pwd):/pdf bwits/pdf2htmlex pdf2htmlEX --fit-width 900 resume.pdf resume.html
+docker run -ti --rm -v $(pwd):/pdf sergiomtzlosa/pdf2htmlex pdf2htmlEX --fit-width 900 resume.pdf resume.html
